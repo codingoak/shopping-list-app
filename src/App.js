21861c5ab61f24882e49a49d50e7ba442cdfa4ba
Daniel Eicher <d.eicher@mailbox.org>
2022-02-11 11:38:58 +0100

    Add create-react-app and styled-components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import styled from "styled-components/macro";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hallo Welt!</header>
+    </div>
+  );
+}
+
+export default App;
